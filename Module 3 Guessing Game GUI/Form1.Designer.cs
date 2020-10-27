8b847f510dfa6a04db7d5953fa008579f85d01ae
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:34:56 -0500

    Add project files.

---
+++ Module 3 Guessing Game GUI/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Module_3_Guessing_Game_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number between 1 and 100";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(696, 73);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(204, 30);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.TextChanged += new System.EventHandler(this.txtNumber_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(696, 520);
+            this.btnGuess.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(375, 92);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Press to Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 703);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnGuess;
+    }
+}
+
---
+++ Module 3 Guessing Game GUI/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module_3_Guessing_Game_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumber_TextChanged(object sender, EventArgs e)
+        {
+            //create a definite loop constant
+            const int MAX_GUESSES = 10;
+
+            //create varibales
+            int system_guess;
+            int user_guess;
+            int counter = 1;
+            bool gotIt = false;
+
+            //create a random number
+            Random random = new Random();
+            system_guess = random.Next(1, 1001);
+
+            //guessing loop
+            while (counter <= MAX_GUESSES)
+            {
+                //ask the user to guess
+                Write("Please enter a number between 1 and 1000: ");
+
+                //retrieve the number from the user
+                user_guess = int.Parse(ReadLine());
+
+                //decide if the user guessed the correct number
+                if (system_guess == user_guess)
+                {
+                    WriteLine($"The number is {user_guess} - you guessed it!");
+                    gotIt = true;
+                    break;
+                }
+                else if (system_guess < user_guess)
+                {
+                    WriteLine("Wrong answer! Go lower!");
+                }
+                else
+                {
+                    WriteLine("Wrong answer! Go higher!");
+                }
+                counter++;
+            }
+
+            if (gotIt == true)
+            {
+                WriteLine("You won! Great job! Bye!");
+            }
+            else
+            {
+                WriteLine($"Sorry you lost the number was {system_guess} - maybe next time - bye!!");
+            }
+        }
+    }
+}
